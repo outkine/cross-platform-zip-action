dc5356858b738da5ee3879d20ebe108dcc36a47c
Anton <antonoutkine@gmail.com>
2020-03-20 18:41:01 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const zip = require('cross-zip')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+  try {
+    zip.zipSync(core.getInput('inPath'), core.getInput('outPath'))
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
